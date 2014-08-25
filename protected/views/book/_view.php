49e09d1869e1654bd4b5e78aa6c5a9f93fc622bf
andyks78 <andrey.strelnik@gmail.com>
2014-08-25 18:37:14 +0400

    CRUD для книг

---
+++ protected/views/book/_view.php
@@ -0,0 +1,25 @@
+<?php
+/* @var $this BookController */
+/* @var $data Book */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
+	<?php echo CHtml::encode($data->name); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('date_create')); ?>:</b>
+	<?php echo CHtml::encode($data->date_create); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('date_edit')); ?>:</b>
+	<?php echo CHtml::encode($data->date_edit); ?>
+	<br />
+
+
+</div>
