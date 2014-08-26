c2a27a313407bc443b57c13e816120c13495d629
andyks78 <andrey.strelnik@gmail.com>
2014-08-26 14:49:40 +0400

    CRUD с под генератора

---
+++ protected/views/reader/_search.php
@@ -0,0 +1,40 @@
+<?php
+/* @var $this ReaderController */
+/* @var $model Reader */
+/* @var $form CActiveForm */
+?>
+
+<div class="wide form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<div class="row">
+		<?php echo $form->label($model,'id'); ?>
+		<?php echo $form->textField($model,'id',array('size'=>20,'maxlength'=>20)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'name'); ?>
+		<?php echo $form->textArea($model,'name',array('rows'=>6, 'cols'=>50)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'date_create'); ?>
+		<?php echo $form->textField($model,'date_create'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'date_edit'); ?>
+		<?php echo $form->textField($model,'date_edit'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Search'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- search-form -->
